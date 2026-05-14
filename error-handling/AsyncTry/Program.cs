84f07d6c5ee4f3368179b56257f47335620afc5f
tsepesh-stack <tsepeshww@gmail.com>
2026-05-14 20:34:13 +0300

    Add error-handling: AsyncTry (try/catch with await basics)

---
+++ error-handling/AsyncTry/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+namespace AsyncTry
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            HttpClient client=new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent","MyApp");
+            client.Timeout = TimeSpan.FromSeconds(5);
+            Stopwatch sw = Stopwatch.StartNew();
+            try{
+                string nb = await client.GetStringAsync("https://httpbin.org/delay/10");
+                Console.WriteLine($"{nb.Length}");}
+            catch (TaskCanceledException)
+            {
+                System.Console.WriteLine("Сервер не ответил во время");
+            }
+            catch (HttpRequestException ex)
+            {
+                System.Console.WriteLine($"Ошибка url: {ex.Message}");
+                Console.WriteLine($"Тип: {ex.GetType().Name}");
+            } System.Console.WriteLine("программа продолжает работать");
+            sw.Stop();
+            System.Console.WriteLine($"Время обработки {sw.ElapsedMilliseconds} мс");
+        }
+    }
+}
