640fd6d81205ef32a4692c18fa8c156081550d1f
tsepesh-stack <tsepeshww@gmail.com>
2026-05-17 18:08:21 +0300

    Initial commit: weather request

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+namespace morningdigest
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent","MyApp");
+            Task<string> we= GetWeatherAsync(client);
+            string weather = await we;
+            System.Console.WriteLine(weather);
+        }
+        static async Task<string> GetWeatherAsync(HttpClient client)
+        {
+            string json = await client.GetStringAsync("https://wttr.in/Zagreb?format=3");
+            return json;
+        }
+    }
+}
