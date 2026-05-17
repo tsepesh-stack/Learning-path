7b1e529c8cb5e1617c49a37ff2cba6dd8b6a7d6a
tsepesh-stack <tsepeshww@gmail.com>
2026-05-17 19:05:07 +0300

    add basics1 and new practic jsonparsing

---
+++ json-parsing/basics1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+namespace basics1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string json = """
+            {
+                "name": "Саша",
+                "age": 23,
+                "isStudent": true
+            }
+            """;
+            var pipl = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+            Person p = JsonSerializer.Deserialize<Person>(json,pipl);
+            string st = p.IsStudent ? "студент" : "не студент";
+            Console.WriteLine($"{p.Name}: {p.Age} лет, {st}");
+        }
+    }
+    class Person
+    {
+        public string Name{get; set;}
+        public int Age{get; set;}
+        public bool IsStudent{get; set;}
+    }
+    
+}
