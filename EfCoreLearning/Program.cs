cf6791507528f3e5b6bf2d696cd2ed762bc841c4
tsepesh-stack <tsepeshww@gmail.com>
2026-07-10 00:18:30 +0300

    EF Core: CRUD операции (Create/Read/Update/Delete) на примере Book

--- EfCoreLearning/Program.cs
+++ EfCoreLearning/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace EfCoreLearning;
+public class Program
+{
+    static async Task Main()
+    {
+        using var db = new AppDbContext();
+        // var book = new Book{Title="Азы программирования", Author="Андрей Столяров", Year=2019};
+        // db.Books.Add(book);
+        // await db.SaveChangesAsync();
+
+        // var allBooks = await db.Books.ToListAsync();
+        // foreach(var el in allBooks)
+        // {
+        // System.Console.WriteLine($"Автор: {el.Author} - книга: {el.Title} {el.Year} года выпуска");
+        // }
+
+        // var book = await db.Books.FindAsync(1);
+        // book.Year = 2020;
+        // await db.SaveChangesAsync();
+
+        
+        var allBooks = await db.Books.ToListAsync();
+        // foreach(var el in allBooks)
+        // {
+        // System.Console.WriteLine($"Автор: {el.Author} - книга: {el.Title} {el.Year} года выпуска");
+        // }
+
+        // var book1 = await db.Books.FindAsync(2);
+        //     db.Books.Remove(book1);
+        //     await db.SaveChangesAsync();
+
+        foreach (var el in allBooks)
+        {
+            db.Books.Remove(el);
+        }
+        await db.SaveChangesAsync();
+        
+
+    }
+}
