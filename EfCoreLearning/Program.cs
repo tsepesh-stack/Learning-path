6941096b6c55e80e6e5b5d59f4649962b39c36b2
tsepesh-stack <tsepeshww@gmail.com>
2026-07-11 23:34:35 +0300

    EF Core: практика CRUD + связи (Book-Author) на новых данных

--- EfCoreLearning/Program.cs
+++ EfCoreLearning/Program.cs
@@ -38,16 +38,70 @@ public class Program
         // await db.SaveChangesAsync();
         
 
-        var author1 = new Author{Name="Столяров"};
+        // var author1 = new Author{Name="Столяров"};
+        // db.Authors.Add(author1);
+        // await db.SaveChangesAsync();
+        
+        // var book1 = new Book{Title="Азы Программирования", Year=2019, AuthorId=author1.Id};
+        // db.Books.Add(book1);
+        // await db.SaveChangesAsync();
+
+        // var books = await db.Books.Include(b => b.Author).ToListAsync();
+        // foreach (var el in books)
+        // Console.WriteLine($"{el.Title} — {el.Author.Name}");
+
+        // var author = new Author{Name="Джек Лондон"};
+        // db.Authors.Add(author);
+        // await db.SaveChangesAsync();
+
+        // var book2 = new Book{Title="Азы Программирования 2", Year=2020, AuthorId=author1.Id};
+        // db.Books.Add(book2);
+        // await db.SaveChangesAsync();
+
+        // var book3 = new Book{Title="Мартин Иден", Year=1999, AuthorId=author.Id};
+        // db.Books.Add(book3);
+        // await db.SaveChangesAsync();
+
+        // var books= await db.Books.Include(b=>b.Author).ToListAsync();
+        // foreach(var el in books)
+        // {
+        //     Console.WriteLine($"{el.Title} — {el.Author.Name}");
+        // }
+
+
+        // var allBooks = await db.Books.ToListAsync();
+        // var allAuthors = await db.Authors.ToListAsync();
+        // foreach(var el in allBooks)
+        // {
+        //     db.Books.Remove(el);
+        // }
+        // foreach(var el in allAuthors)
+        // {
+        //     db.Authors.Remove(el);
+        // }
+        // await db.SaveChangesAsync();
+
+        var author1 = new Author{Name="Джордж Оруэлл"};
         db.Authors.Add(author1);
+        var author2 = new Author{Name="Рэй Брэдбери"};
+        db.Authors.Add(author2);
         await db.SaveChangesAsync();
         
-        var book1 = new Book{Title="Азы Программирования", Year=2019, AuthorId=author1.Id};
+        var book1 = new Book{Title="1984", AuthorId=author1.Id, Year=1949};
         db.Books.Add(book1);
+        var book2 = new Book{Title="Скотный двор", AuthorId=author1.Id,Year= 1945};
+        db.Books.Add(book2);
+        var book3 = new Book{Title="451 градус по Фаренгейту",AuthorId=author2.Id, Year=1953};
+        db.Books.Add(book3);
         await db.SaveChangesAsync();
 
-        var books = await db.Books.Include(b => b.Author).ToListAsync();
-        foreach (var el in books)
-        Console.WriteLine($"{el.Title} — {el.Author.Name}");
+        var bookUp= await db.Books.FindAsync(book1.Id);
+        bookUp.Year = 1948;
+        await db.SaveChangesAsync();
+
+        var bookDel = await db.Books.FindAsync(book2.Id);
+        db.Books.Remove(bookDel);
+        await db.SaveChangesAsync();
+
     }
 }
